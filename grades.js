6e7c00eef2adb24680acd362a6ad22980e36395e
Kaustubh Mani <106481862+Kaustubh022@users.noreply.github.com>
2022-12-17 17:15:58 +0530

    Delete grades.js

--- grades.js
+++
@@ -1,2 +0,0 @@
-grades=[{grade:"AA",value:10},{grade:"AB",value:9},{grade:"BB",value:8},{grade:"BC",value:7},{grade:"CC",value:6},{grade:"CD",value:5},{grade:"DD",value:4},{grade:"FF",value:0},{grade:"II",value:0}];
-//Grade : AA - 10 AB - 9 BB - 8 BC - 7 CC - 6 CD - 5 DD - 4 FF - 0 II - Incomplete
